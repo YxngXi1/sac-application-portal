712f90fdff2405cf29b848c05cfdaf6b5f264cc0
Harshad2009 <harshadmahajan09@gmail.com>
2026-09-07 00:19:39 -0400

    updated close date

--- src/lib/applicationSchedule.ts
+++ src/lib/applicationSchedule.ts
@@ -1,5 +1,5 @@
 export const APPLICATION_OPEN_DATE = new Date('2026-09-09T15:00:00-04:00');
-export const APPLICATION_CLOSE_DATE = new Date('2027-09-14T23:59:00-04:00');
+export const APPLICATION_CLOSE_DATE = new Date('2026-09-14T23:59:00-04:00');
 
 export const APPLICATION_OPEN_LABEL = 'September 9th, 2026 at 3:00 PM EDT';
 export const APPLICATION_CLOSE_LABEL = 'September 14th, 2026 at 11:59 PM ET';
